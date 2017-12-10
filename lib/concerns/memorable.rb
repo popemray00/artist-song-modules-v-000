f380eeeb88c903a72837143f9e09ec562e58cc2a
Michael Pope <popemray1986@gmail.com>
2017-12-10 18:16:31 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,17 @@
 module Memorable
   module ClassMethods
-  def self.reset_all
-    self.all.clear
-  end
+    def self.reset_all
+      self.all.clear
+    end
 
-  def self.count
-    self.all.count
-  end
+    def self.count
+      self.all.count
+    end
 end
+
+  module InstanceMethods
+    def method_name
+
+    end
+  end
 end
