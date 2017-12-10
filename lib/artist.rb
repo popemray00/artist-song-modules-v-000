55d60f2a56ab8f72429665d6b40d26db8ae1faa6
Michael Pope <popemray1986@gmail.com>
2017-12-10 18:24:55 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,10 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+  
   attr_accessor :name
   attr_reader :songs
 
